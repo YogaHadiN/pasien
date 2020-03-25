0744816fb6cf6428b2adf552d1a70c903bf6a69a
YogaHadiN <yoga_email@yahoo.com>
2020-03-25 12:56:48 +0700

    desain monitor

--- resources/views/monitor.blade.php
+++ resources/views/monitor.blade.php
@@ -1,93 +1,109 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-        <title>Laravel</title>
+<head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+  <title>Bare - Start Bootstrap Template</title>
 
-            .full-height {
-                height: 100vh;
-            }
+  <!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<style type="text/css" media="all">
+	html, body {
+		background-color: #fff;
+		color: #636b6f;
+		font-family: 'Nunito', sans-serif;
+		font-weight: 200;
+		height: 100vh;
+		margin: 0;
+	}
+	.row div{
+		border: 1px solid black;
+	}
+	.superbig{
+		font-size: 200px;
+		padding : 100 50 !important;
+	}
+	.big{
+		font-size: 100px;
+		padding : 100 50 !important;
+	}
+	.list {
+		font-size : 50px;
+	}
+</style>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+</head>
 
-            .position-ref {
-                position: relative;
-            }
+<body>
+  <!-- Page Content -->
+  <div class="container">
+    <div class="row text-center">
+      <div class="col-lg-8">
+		<h2>Pasien Dipanggil</h2>
+			<div id="nomor_antrian" class="superbig">A1</div>
+		<h2>Poli Umum</h2>
+      </div>
+		<div class="col-lg-4">
+			<h2>Pasien Diperiksa</h2>
+			<div class="">
+				<div class="list">A1 / 10</div>
+				<div class="list">B2 / 10</div>
+				<div class="list">C5 / 10</div>
+				<div class="list">D9 / 10</div>
+			</div>
+      </div>
+    </div>
+	<div class="row text-center">
+		<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+			<h2>Pasien Diperiksa</h2>
+				<div id="nomor_antrian" class="big">A1</div>
+			<h2>Poli Umum</h2>
+		</div>
+		<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+			<h2>Pasien Diperiksa</h2>
+				<div id="nomor_antrian" class="big">B2</div>
+			<h2>Poli Gigi</h2>
+			
+		</div>
+		<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+			<h2>Pasien Diperiksa</h2>
+				<div id="nomor_antrian" class="big">C5</div>
+			<h2>Poli Kebidanan</h2>
+			
+		</div>
+		<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+			<h2>Pasien Diperiksa</h2>
+				<div id="nomor_antrian" class="big">D9</div>
+			<h2>Poli Estetik</h2>
+			
+		</div>
+	</div>
+  </div>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+  <!-- Bootstrap core JavaScript -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script src="https://js.pusher.com/5.1/pusher.min.js"></script>
+<script>
+	// Enable pusher logging - don't include this in production
+	Pusher.logToConsole = true;
 
-            .content {
-                text-align: center;
-            }
+	var pusher = new Pusher('281b6730814874b6b533', {
+	  cluster: 'ap1',
+	  forceTLS: true
+	});
+	var channel = pusher.subscribe('monitor-channel');
+	channel.bind('monitor-submitted', function(data) {
+		$('#nomor_antrian').html(data.text);
+	});
+</script>
 
-            .title {
-                font-size: 84px;
-            }
+</body>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-				<h1>KLINIK JATI ELOK</h1>
-				<h2>Pasien Diperiksa</h2>
-                <div id="nomor_antrian" class="title m-b-md">{{ $antrian }}</div>
-				{{-- <h1>Untuk melaporkan kesalahan : </h1> --}}
-				{{-- <h1> <a href="http://bit.ly/39WV083"> Klik Disini</a>  </h1> --}}
-            </div>
-        </div>
-		  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-		 <script src="https://js.pusher.com/5.1/pusher.min.js"></script>
-		  <script>
-
-			// Enable pusher logging - don't include this in production
-			Pusher.logToConsole = true;
-
-			var pusher = new Pusher('281b6730814874b6b533', {
-			  cluster: 'ap1',
-			  forceTLS: true
-			});
-			var channel = pusher.subscribe('monitor-channel');
-			channel.bind('monitor-submitted', function(data) {
-				$('#nomor_antrian').html(data.text);
-			});
-		  </script>
-    </body>
 </html>
