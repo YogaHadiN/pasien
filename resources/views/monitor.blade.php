1fa037b166ab8d5526eb8511b3e8c2adc8d43fbb
YogaHadiN <yoga_email@yahoo.com>
2020-03-25 13:40:14 +0700

    monitor updated

--- resources/views/monitor.blade.php
+++ resources/views/monitor.blade.php
@@ -14,14 +14,6 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 <style type="text/css" media="all">
-	html, body {
-		background-color: #fff;
-		color: #636b6f;
-		font-family: 'Nunito', sans-serif;
-		font-weight: 200;
-		height: 100vh;
-		margin: 0;
-	}
 	.row div{
 		border: 1px solid black;
 	}
@@ -36,6 +28,26 @@
 	.list {
 		font-size : 50px;
 	}
+	.full-height
+	{
+		height: 100%;
+	}
+	html, body {
+		background-color: #fff;
+		color: #636b6f;
+		font-family: 'Nunito', sans-serif;
+		font-weight: 200;
+		height: 100vh;
+		margin: 0;
+	}
+	.row{
+		overflow: hidden; 
+	}
+
+	[class*="col-"]{
+		margin-bottom: -99999px;
+		padding-bottom: 99999px;
+	}
 </style>
 
 </head>
@@ -44,44 +56,48 @@
   <!-- Page Content -->
   <div class="container">
     <div class="row text-center">
-      <div class="col-lg-8">
-		<h2>Pasien Dipanggil</h2>
-			<div id="nomor_antrian" class="superbig">A1</div>
-		<h2>Poli Umum</h2>
-      </div>
-		<div class="col-lg-4">
+		<div class="col-lg-8 no-float">
+			<h2>Pasien Dipanggil</h2>
+				<div id="nomor_antrian" class="superbig">A1</div>
+			<h2>Poli Umum</h2>
+		</div>
+		<div class="col-lg-4 full-height">
 			<h2>Pasien Diperiksa</h2>
-			<div class="">
+			<div>
 				<div class="list">A1 / 10</div>
 				<div class="list">B2 / 10</div>
 				<div class="list">C5 / 10</div>
 				<div class="list">D9 / 10</div>
 			</div>
-      </div>
+		</div>
     </div>
 	<div class="row text-center">
-		<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+		<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 no-float">
 			<h2>Pasien Diperiksa</h2>
 				<div id="nomor_antrian" class="big">A1</div>
 			<h2>Poli Umum</h2>
 		</div>
-		<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+		<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 no-float">
 			<h2>Pasien Diperiksa</h2>
 				<div id="nomor_antrian" class="big">B2</div>
 			<h2>Poli Gigi</h2>
 			
 		</div>
-		<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+		<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 no-float">
 			<h2>Pasien Diperiksa</h2>
 				<div id="nomor_antrian" class="big">C5</div>
 			<h2>Poli Kebidanan</h2>
 			
 		</div>
-		<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+		<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 no-float">
 			<h2>Pasien Diperiksa</h2>
 				<div id="nomor_antrian" class="big">D9</div>
 			<h2>Poli Estetik</h2>
 			
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 		</div>
 	</div>
   </div>
